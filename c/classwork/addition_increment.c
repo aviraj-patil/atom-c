b3786f9f0309e9167949851d19e29669c0a3dc6e
aviraj-patil <avirajp7733@gmail.com>
2021-04-12 22:16:51 +0530

    code01

---
+++ c/classwork/addition_increment.c
@@ -0,0 +1,22 @@
+/*write algorithm and flowchart*/
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+ {
+   int a=1,b=1,sum1,sum2;
+
+printf("%d\n%d\n", a,b);
+
+
+while (sum2<100) {
+  sum1=a+b;
+  a=sum1;
+  printf("%d\n", a);
+  sum2=a+b;
+  b=sum2;
+  printf("%d\n", b);
+
+}
+
+}
